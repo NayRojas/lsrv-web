eafd7a031915a33237664e46f63ce83153c97ce3
Nayllen Soleika Rojas Martinez <nrojasma@LIM-T29KQPXG73.local>
2024-03-13 23:30:03 -0500

    feat: update general styles

--- softball-web/src/app/core/components/header/header.component.ts
+++ softball-web/src/app/core/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  isMenuVisible: boolean = false;
 
+  constructor(private router: Router) {
+
+  }
+
+  toggleMenu() {
+    this.isMenuVisible = !this.isMenuVisible;
+  }
+
+  goToHome() {
+    this.router.navigateByUrl('/inicio');
+  }
 }
