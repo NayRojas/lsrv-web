99470180e0642e5fc51b87cd64f0cb0116a661c3
Nayllen Soleika Rojas Martinez <nrojasma@LIM-T29KQPXG73.local>
2024-03-15 23:14:22 -0500

    feat: update modules

---
+++ softball-web/src/app/features/results/results.presenter.spec.ts
@@ -0,0 +1,7 @@
+import { ResultsPresenterTs } from './results.presenter';
+
+describe('ResultsPresenterTs', () => {
+  it('should create an instance', () => {
+    expect(new ResultsPresenterTs()).toBeTruthy();
+  });
+});
